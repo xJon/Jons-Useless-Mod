2cc22e2b803aa8d6b5b09f40c2055afc169407a1
Jon <elnatan70@gmail.com>
2015-10-04 14:53:20 +0300

    Changed mod version

--- src/main/java/xjon/jum/util/Reference.java
+++ src/main/java/xjon/jum/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "jum";
 	public static final String MOD_NAME = "Jon's Useless Mod";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String CLIENT_PROXY_CLASS = "xjon.jum.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "xjon.jum.proxy.CommonProxy";
 	public static final String GUI_FACTORY = "xjon.jum.client.gui.UselessGuiFactory";
